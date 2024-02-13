28a52e0d3daedb7df742065a5f2c77f0b9bad516
Brandon Zhang <brandonzhang@Brandons-MacBook-Air.local>
2024-02-12 19:27:57 -0500

    Added login authentication route and middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import ToasterContext from '@/app/context/ToasterContext';
+import AuthContext from '@/app/context/AuthContext';
 
 import "./globals.css";
 
@@ -20,8 +21,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   );
