aaa503b1c82c30596623730072c9f578a92458c5
Brandon Zhang <brandonzhang@Brandons-MacBook-Air.local>
2024-02-17 08:37:40 -0500

    Fixed spacing for css

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -30,7 +30,8 @@ const Input: React.FC<InputProps> = ({ label, id, type, required, register, erro
           disabled={disabled}
           {...register(id, { required })}
           className={cn(
-            "form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6",
+            "form-input block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300",
+            "placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6",
             errors[id] && "focus:ring-rose-500",
             disabled && "opacity-50 cursor-default"
           )}
