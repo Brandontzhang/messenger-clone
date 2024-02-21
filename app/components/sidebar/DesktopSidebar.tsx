78e7fd465a49bf679acd74c4ec362a1f39b89f2e
Brandon Zhang <brandonzhang@Brandons-MacBook-Air.local>
2024-02-21 18:15:25 -0500

    Adjust css spacing

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import Avatar from "../Avatar";
 import DesktopItem from "./DesktopItem";
 import { User } from "@prisma/client";
+import { cn } from "@/util/common";
 
 interface DesktopSidebarProps {
   currentUser: User | null
@@ -17,7 +18,10 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
 
   return (
-    <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
+    <div className={cn(
+      "hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white",
+      "lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between"
+    )}>
       <nav className="mt-4 flex flex-col justify-between">
         <ul role="list" className="flex flex-col items-center space-y-1">
           {routes.map((item) => (
