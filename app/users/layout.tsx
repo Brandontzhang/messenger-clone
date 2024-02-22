e1b49bd0ea3a734418875eb3a7427b576d36d71b
Brandon Zhang <brandonzhang@Brandons-MacBook-Air.local>
2024-02-21 19:15:50 -0500

    Added people side list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,9 +1,14 @@
 import Sidebar from "@/app/components/sidebar/Sidebar";
+import UserList from "./components/UserList";
+import getUsers from "../actions/getUsers";
 
 const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     <Sidebar>
       <div className="h-full">
+        <UserList items={users} />
         {children}
       </div>
     </Sidebar>
